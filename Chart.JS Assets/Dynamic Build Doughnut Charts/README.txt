5d2e0b1a4dea6f77438039b47f0516ef1e987626
Broderick <BRODW2A@TDBFG.COM>
2019-01-11 09:00:46 -0500

    added half radial progress bar function

--- Chart.JS Assets/Dynamic Build Doughnut Charts/README.txt
+++ Chart.JS Assets/Dynamic Build Doughnut Charts/README.txt
@@ -1,5 +1,3 @@
 A Pen created at CodePen.io. You can find this one at https://codepen.io/BroderickWI/pen/MZqYMY.
 
  
-
-Allows one to pass the label and the percentage (2 variables) to a javascript function responsible for returning a rendered Doughnut Chart with tweakable specifications.
--- Chart.JS Assets/Dynamic Build Doughnut Charts/js/index.js
+++ Chart.JS Assets/Dynamic Build Doughnut Charts/js/index.js
@@ -1,27 +1,28 @@
-setupChart('Aboriginal', 33);
-setupChart('PWD', 66);
-setupChart('Women', 99);
+fullDoughnutChart('Aboriginal', 33);
+fullDoughnutChart('PWD', 66);
+fullDoughnutChart('Women', 99);
+halfDonughnutChart('Test', 33);
 
-function setupChart(chartId, progress) {
+function fullDoughnutChart(chartId, progress) {
   var canvas = document.getElementById(chartId);
   var context = canvas.getContext('2d');
 
   var remaining = 100 - progress;
   var data = {
-    labels: [ 'Progress', '', ],
+    labels: [ 'Current', 'Future', ],
     datasets: [{
-      data: [progress, remaining],
+      data: [progress , remaining],
       backgroundColor: [
         '#1A5336',
         '#008A00'
       ],
       borderColor: [
-        '#1A5336',
-        '#008A00'
+        '#FFFFFF',
+        '#FFFFFF'
       ],
       hoverBackgroundColor: [
-        '#008A00',
-        '#1A5336'
+        '#105b34',
+        '#008a2e'
       ]
     }]
   };
@@ -47,10 +48,73 @@ function setupChart(chartId, progress) {
         var progressLabel = data.datasets[0].data[0] + '%';
          var targetPercent = 100;
          var label = chartId;
-        context.font = '20px Helvetica';
+        context.font = '22px Thasadith';
         context.fillStyle = 'black';
         context.fillText(progressLabel, xCenter, yCenter-10);
-        context.fillText("x% of Target", xCenter, yCenter+10);
+        context.fillText(chartId, xCenter, yCenter+10);
+      }
+    }
+  };
+
+  Chart.defaults.global.tooltips.enabled = true;
+  var chart = new Chart(context, {
+    type: 'doughnut',
+    data: data,
+    options: options
+  });
+}
+
+function halfDonughnutChart(chartId, progress) {
+  var canvas = document.getElementById(chartId);
+  var context = canvas.getContext('2d');
+
+  var remaining = 100 - progress;
+  var data = {
+    labels: [ 'Current', 'Future', ],
+    datasets: [{
+      data: [progress , remaining],
+      backgroundColor: [
+        '#1A5336',
+        '#008A00'
+      ],
+      borderColor: [
+        '#FFFFFF',
+        '#FFFFFF'
+      ],
+      hoverBackgroundColor: [
+        '#105b34',
+        '#008a2e'
+      ]
+    }]
+  };
+
+  var options = {
+    responsive: false,
+    maintainAspectRatio: false,
+    scaleShowVerticalLines: false,
+    rotation: 1 * Math.PI,
+    circumference: 1 * Math.PI,
+
+    cutoutPercentage: 70,
+    legend: {
+      display: false
+    },
+    animation: {
+      onComplete: function (event) {
+      	console.log(this.chart.height);
+        var xCenter = this.chart.width/2;
+        var yCenter = this.chart.height/2;
+
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+
+        var progressLabel = data.datasets[0].data[0] + '%';
+         var targetPercent = 100;
+         var label = chartId;
+        context.font = '22px Thasadith';
+        context.fillStyle = 'black';
+        context.fillText(progressLabel, xCenter+3, yCenter*1.3);
+        context.fillText(chartId, xCenter, yCenter*1.5);
       }
     }
   };
