5f776edafaa738b8f2f4d747c6b7173862207b80
Waun Broderick <waunbroderick@gmail.com>
2019-01-16 22:34:27 -0500

    Variables for passed projection init render, and html DOM manipulation logic

--- Projection Tool/js/index.js
+++ Projection Tool/js/index.js
@@ -23,8 +23,13 @@ ctx.arc(xCenter,yCenter,cSize,0,2*Math.PI);
 ctx.stroke();
 
 ctx.font='14px FontAwesome';
-ctx.fillText(totHeadCountCurrent, xCenter-15, yCenter-40);
+ctx.fillText(totHeadCountCurrent, xCenter-15, yCenter-60);
+ctx.fillText("Total Employees", xCenter-45, yCenter-40);
+  
 ctx.fillText("\uf508",20,50);
 ctx.fillText("Total Head Count Future: " + totHeadCountFuture,20,70);
 ctx.fillText("Required Hires: " + RequiredHires,20,90);
+  
+  
+document.getElementById("param1").innerHTML = RequiredHires;
 }
